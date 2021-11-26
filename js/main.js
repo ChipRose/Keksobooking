30e635b0613ea109b92450938ba1d0e120b9b602
Yana Rozova <yana.rozova@mail.ru>
2021-11-26 17:58:07 +0300

    generate random number function created

--- js/main.js
+++ js/main.js
@@ -1,26 +1,41 @@
 'use strict';
 
-let getRandomTemplate = function (min, max, accuracy) {
-  if (min >= 0 && max > 0 && max >= min && accuracy >= 0) {
-    if (Math.floor(min) === Math.floor(max) && max !== min && accuracy === 0) {
-      return -1;
-    }
-    let power = Math.pow(10, accuracy);
-    min = Math.ceil(min * power);
-    max = Math.floor(max * power + 1);
-    return Math.floor((Math.random() * (max - min) + min)) / power;
-  } else {
+let getRandomNumber = (min, max, accuracy) => {
+  if (min < 0 || max < 0 || accuracy < 0) {
     return -1;
   }
+
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+
+  min = parseFloat(min);
+  max = parseFloat(max);
+  accuracy = parseInt(accuracy);
+
+  if (isNaN(min) || isNaN(max) || isNaN(accuracy)) {
+    return -1;
+  }
+
+  let power = Math.pow(10, accuracy);
+  min = Math.floor(min * power);
+  max = Math.floor(max * power + 1);
+  return Math.floor(Math.random() * (max - min) + min) / power;
 };
 
-let getRandomInt = function (min, max) {
-  return getRandomTemplate(min, max, 0);
+let getRandomInt = (min, max) => {
+  return getRandomNumber(min, max, 0);
 };
 
-let getRandomFloat = function (min, max, accuracy) {
-  return getRandomTemplate(min, max, accuracy);
-}
+let getRandomFloat = (min, max, accuracy) => {
+  if (Math.floor(min) === Math.floor(max) && accuracy === 0) {
+    if (Math.floor(min) !== min && Math.floor(max) !== max) {
+      return -1;
+    }
+  }
 
-getRandomInt(6, 6);
-getRandomFloat(2.1, 2.5, 1);
+  return getRandomNumber(min, max, accuracy);
+};
+
+getRandomInt(4, 4);
+getRandomFloat(3.2, 3.8, 0);
