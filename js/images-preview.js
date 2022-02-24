121861f754486cfe35f947ea41e27d6cb56fc597
Yana Rozova <yana.rozova@mail.ru>
2022-02-25 01:52:30 +0300

    Added proto preview

--- js/images-preview.js
+++ js/images-preview.js
@@ -1,11 +1,12 @@
 const FILE_TYPES = ['.gif', '.jpg', '.jpeg', '.png'];
 
 const promoForm = document.querySelector('.ad-form');
-const avatarInput = promoForm.querySelector('#avatar');
-const avatarPreview = promoForm.querySelector('.ad-form-header__preview img');
-const avatarPreviewContainer = promoForm.querySelector('.ad-form-header__preview');
+const avatarInput = promoForm.querySelector('.ad-form-header__input');
+const avatarPreview = promoForm.querySelector('.ad-form-header__preview');
+const photoInput = promoForm.querySelector('.ad-form__input');
+const photoPreview = promoForm.querySelector('.ad-form__photo');
 
-avatarInput.addEventListener('change', () => {
+/* avatarInput.addEventListener('change', () => {
   const file = avatarInput.files[0];
   const fileName = file.name.toLowerCase();
 
@@ -13,17 +14,58 @@ avatarInput.addEventListener('change', () => {
     return fileName.endsWith(extension)
   });
 
+  avatarPreviewContainer.innerHTML = '';
+
   if (matches) {
     const reader = new FileReader();
 
     reader.addEventListener('load', () => {
-      avatarPreview.src = reader.result;
-      avatarPreviewContainer.style.padding = '5px';
-      avatarPreviewContainer.style.overFlow = 'hidden';
-      avatarPreview.width = '60';
-      avatarPreview.height = '60';
+      const avatar = document.createElement('img');
+      avatarPreviewContainer.style.width = '200px';
+      avatarPreviewContainer.style.height = '70px';
+      avatarPreviewContainer.style.justifyContent = 'center';
+      avatarPreviewContainer.style.padding = '0';
+      avatarPreviewContainer.style.overflow = 'hidden';
+      avatar.src = reader.result;
+      avatarPreviewContainer.appendChild(avatar);
     });
 
     reader.readAsDataURL(file);
   }
+}); */
+
+avatarInput.addEventListener('change', () => {
+  setImagePreview(avatarInput, avatarPreview);
 });
+
+photoInput.addEventListener('change', () => {
+  setImagePreview(photoInput, photoPreview);
+});
+
+const setImagePreview = (inputField, previewField) => {
+  const file = inputField.files[0];
+  const fileName = file.name.toLowerCase();
+
+  const matches = FILE_TYPES.some((extension) => {
+    return fileName.endsWith(extension)
+  });
+
+  previewField.innerHTML = '';
+
+  if (matches) {
+    const reader = new FileReader();
+
+    reader.addEventListener('load', () => {
+      const image = document.createElement('img');
+      image.src = reader.result;
+      previewField.appendChild(image);
+      previewField.style.padding = '0';
+      previewField.style.display = 'flex';
+      previewField.style.justifyContent = 'center';
+      previewField.style.alignItems = 'center';
+      previewField.style.overflow = 'hidden';
+    });
+
+    reader.readAsDataURL(file);
+  }
+}
